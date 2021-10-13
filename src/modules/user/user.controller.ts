371026a90b868c902d1b603bae7e956b88d43c60
Andre Macedo <andre.macedo@seidor.com.br>
2021-10-13 16:32:45 -0300

    feat: add get by id

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -34,7 +34,7 @@ export class UserController {
 
   @Get(':id')
   async findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.findOne(id);
   }
 
   @Patch(':id')
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -17,8 +17,8 @@ export class UserService {
     return this.repository.find();
   }
 
-  async findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    return this.repository.findOne(id);
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {

ec0131b4c1cce113addf9a9894113cd434987da1
Andre Macedo <andre.macedo@seidor.com.br>
2021-10-14 11:20:17 -0300

    test: fix service tests

--- src/modules/user/services/user.service.spec.ts
+++ src/modules/user/services/user.service.spec.ts
@@ -1,18 +1,36 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+import UserRepository from '../repositories/user.repository';
 import { UserService } from './user.service';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService;
+  let userRepository;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserRepository),
+          useValue: {
+            find: jest.fn(),
+            findAll: jest.fn(),
+            delete: jest.fn(),
+            save: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<UserService>(UserService);
+    userService = moduleRef.get<UserService>(UserService);
+    userRepository = await moduleRef.resolve<UserRepository>(
+      getRepositoryToken(UserRepository),
+    );
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(userService).toBeDefined();
   });
 });
